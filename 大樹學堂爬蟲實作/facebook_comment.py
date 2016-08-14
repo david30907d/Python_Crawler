d05c79ab5044710178c78d9e970b85c942d57191
張泰瑋(Chang Tai Wei) <davidtnfsh@gmail.com>
2016-08-14 20:09:18 +0800

    把自己的access_token拿掉

--- 大樹學堂爬蟲實作/facebook_comment.py
+++ 大樹學堂爬蟲實作/facebook_comment.py
@@ -1,5 +1,5 @@
 import requests, json
-token = 'EAACEdEose0cBAEhKJ8RVwDvgAiP8GJngy72pXV6YHqOIJZBwze10mRh0Le1SvKZBfJhn2XshpytZBdpZBqnDoRFusZB0ZC81DQQjhV3vd4mFct3MIEKeXHnZCZCGZBMV4NdN9G8XnXf0kirfW26qjnfrECLl5pbk6ZB15ESFFXYGnTbgZDZD'
+token = "這邊要放自己的臉書access_token"
 re = requests.get('https://graph.facebook.com/v2.5/me/posts?limit=1&access_token=' + token)# api用post就會顯示該使用者的貼文s
 #if you want to chose a specific time span, append 'since=1420041600' after 'post?' 
 #cause it use Unix seconds, so 1420041600 means 2015/01/01
