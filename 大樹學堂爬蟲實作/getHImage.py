5c2bc937a090b5e52a9000a00776dc69cda57765
張泰瑋(Chang Tai Wei) <davidtnfsh@gmail.com>
2017-01-01 19:34:41 +0800

    建立資料夾

--- 大樹學堂爬蟲實作/getHImage.py
+++ 大樹學堂爬蟲實作/getHImage.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8 -*-
 import requests, json
 from bs4 import BeautifulSoup
-import shutil
-import os
+import shutil, subprocess, os
 
 path = input("Please input the path you want to store these image:")
 try:
+    subprocess.call(['mkdir', '-p', path])
     os.chdir(path)
-    #means cd path
 except:
     print('path error')
 targetURL = input("Please input the url that you want to Parse:")
 res =requests.get(targetURL)
+res.encoding =  res.apparent_encoding
 soup = BeautifulSoup(res.text)
 for index, i in enumerate(soup.select('img')):
     try:
@@ -25,3 +25,14 @@ for index, i in enumerate(soup.select('img')):
         del ires
     except Exception as e:
         print(i)
+
+headline = soup.select('h1.head')[0]
+headline = headline.text.replace('\'','')
+headline = headline.replace('$','')
+headline = headline.replace('\n','')
+headline = headline.replace(' ','')
+headline = headline.replace('/','')
+headline = headline.replace('\u3000','')
+os.chdir('../')
+print(os.getcwd())
+subprocess.call(['mv', path, headline])
